c929be1b655ca454c36d8ae7b89844434fdba104
BhjrajCoditas <bhojraj.narware@coditas.com>
2025-07-08 13:36:08 +0530

    week 1 question 1 done

---
+++ week1/ques1.kt
@@ -0,0 +1,9 @@
+
+// 1. Declare a variable named 'message' and assign the value "Hello, Kotlin!" to it.
+
+fun main() {
+    val message: String = "Hello, Kotlin!!"
+    println(message)
+}
+
+// solution link: https://play.kotlinlang.org/embed?short=XLea-VUJ2&theme=darcula
