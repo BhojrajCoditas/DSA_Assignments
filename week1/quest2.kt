347ad960876bf58ff9cd3e2e7d0d64d62555d00a
BhjrajCoditas <bhojraj.narware@coditas.com>
2025-07-08 14:57:05 +0530

    week 1 question no. 2 done

---
+++ week1/quest2.kt
@@ -0,0 +1,16 @@
+/*2. Write a Kotlin program that checks if a given number is even or odd.
+   Input: 2
+   Output: 2 is even
+*/
+
+fun main() {
+    val inputNum: Int = 2
+    if(inputNum%2 == 0){
+        println("$inputNum is even.")
+    } else {
+        println("$inputNum is odd.")
+    }
+}
+
+// solution link: https://play.kotlinlang.org/embed?short=NQlPmKxq-&theme=darcula
+
