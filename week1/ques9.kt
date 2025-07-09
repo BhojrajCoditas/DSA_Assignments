c7db944ce1329e8bc4cb9d2e4f5cdd1e6d36cbe6
BhjrajCoditas <bhojraj.narware@coditas.com>
2025-07-09 13:44:16 +0530

    solved the week1 question no. 9

---
+++ week1/ques9.kt
@@ -0,0 +1,23 @@
+/*
+9. Write a Kotlin program that checks whether a given number is positive, negative, or zero.
+   Input: 7
+   Output: Positive
+
+*/
+
+
+fun main() {
+    // user input usecase
+    //var num = readln().toInt()
+    
+    var num = 0
+    
+    if(num>0){
+        println("Positive")
+    } else if(num ==0){
+        println("Zero")
+    }else{
+        println("negative")
+    }
+}
+
